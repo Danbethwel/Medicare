e4ddb390af4e4f96f8003eae7eb7d3442b4b44bc
Danbethwel <93147941+Danbethwel@users.noreply.github.com>
2023-07-16 16:45:07 +0300

    new commit

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import userImg from '../../assets/images/avatar-icon.png';
 import logo from '../../assets/images/logo.png';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { BiMenu } from 'react-icons/bi';
 
 const navlinks = [
   {
@@ -22,31 +24,70 @@ const navlinks = [
 ];
 
 const Header = () => {
-  const isActive = true; // Assuming this variable determines the active state of the link
+  const headerRef = useRef(null);
+  const menuRef = useRef(null); // Add menuRef
+
+  const handleStickyHeader = () => {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      headerRef.current.classList.add('sticky__header');
+    } else {
+      headerRef.current.classList.remove('sticky__header');
+    }
+  };
+
+  useEffect(() => {
+    handleStickyHeader();
+    
+    window.addEventListener('scroll', handleStickyHeader); // Add scroll event listener
+
+    return () => window.removeEventListener('scroll', handleStickyHeader);
+  }, [handleStickyHeader]); // Add handleStickyHeader as a dependency
+
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu');
 
   return (
     <header className='header'>
       <div className='container'>
-        <div className='flex items-center justify-between'>
+        <div className='flex items-center justify-between' ref={headerRef}>
           {/* =========== logo ========= */}
           <div>
             <img src={logo} alt='Logo' />
           </div>
 
           {/* =========== menu ========= */}
-          <div className='navigation'>
+          <div className='navigation' ref={menuRef} onClick={toggleMenu}>
             <ul className='menu flex items-center gap-[2.7rem]'>
               {navlinks.map((link, index) => (
                 <li key={index}>
                   <NavLink
                     to={link.path}
-                    className={isActive ? 'text-primaryColor text-[16px] leading-7 font-[600]' : 'text-textColor text-[16px] leading-7 font-[500]'}
+                    className='text-primaryColor text-[16px] leading-7 font-[600]'
                   >
                     {link.display}
                   </NavLink>
                 </li>
               ))}
             </ul>
+          </div>
+
+          {/* =========== nav right ========= */}
+          <div className='flex items-center gap-4'>
+            <div className='hidden'>
+              <Link to='/'>
+                <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
+                  <img src={userImg} className='w-full rounded-full' alt='' />
+                </figure>
+              </Link>
+            </div>
+
+            <Link to='/login'>
+              <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center rounded-[58px]'>
+                Login
+              </button>
+            </Link>
+            <span className='md:hidden' onClick={toggleMenu}>
+              <BiMenu className='w-6 h-6 cursor-pointer' />
+            </span>
           </div>
         </div>
       </div>
@@ -55,4 +96,3 @@ const Header = () => {
 };
 
 export default Header;
-

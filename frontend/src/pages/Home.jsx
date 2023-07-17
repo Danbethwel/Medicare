14db6ed4a1f9a5cf3ab8631523d51d35cc2cae8c
Danbethwel <93147941+Danbethwel@users.noreply.github.com>
2023-07-17 13:40:05 +0300

    new commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -39,19 +39,19 @@ const Home = () => {
                 <div>
                   <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700]
                   text-headingColor'>
-                    30+
+                    15+
                   </h2>
                   <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]' ></span>
-                  <p className='text__para'>Years of Experience</p>
+                  <p className='text__para'>Clinics</p>
                 </div>
 
                 <div>
                   <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700]
                   text-headingColor'>
-                    30+
+                    100%
                   </h2>
                   <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]' ></span>
-                  <p className='text__para'>Years of Experience</p>
+                  <p className='text__para'>Customer Satisfaction</p>
                 </div>
 
               </div>
